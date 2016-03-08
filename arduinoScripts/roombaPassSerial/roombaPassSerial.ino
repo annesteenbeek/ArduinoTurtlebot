d560905c131cee205da19b1052ff9668fa8872b1
annesteenbeek <annesteenbeek@gmail.com>
2016-03-08 19:17:08 +0100

    changes

--- arduinoScripts/roombaPassSerial/roombaPassSerial.ino
+++ arduinoScripts/roombaPassSerial/roombaPassSerial.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 SoftwareSerial pinSerial(5, 6); //rx,tx
 int ddPin = 7;
- 
+
 void setup() {
   pinMode(ddPin,  OUTPUT);
   pinMode(13, OUTPUT);
@@ -13,15 +13,17 @@ void setup() {
  
 void loop() {
   // Send PC data to roomba
-  if (Serial.available()>0) {
-    char outByte = Serial.read();
-    pinSerial.write(outByte); // send received data as binary data to Roomba
+  while (Serial.available()>0) {
+    int outByte = Serial.parseInt();
+    pinSerial.write((byte)outByte); // send received data as binary data to Roomb
+    Serial.print("Send to roomba: "); Serial.println(outByte);
   }
 
   // Send received data back to PC
-  if (pinSerial.available() > 0) {
-    char inByte = pinSerial.read();
-    Serial.write(inByte);
+  while (pinSerial.available() > 0) {
+    byte inByte = pinSerial.read();
+    Serial.print("Received from roomba: ");
+    Serial.println(inByte);
   }
 }
 
@@ -32,16 +34,4 @@ void startRoomba(){
   digitalWrite(ddPin, LOW);
   delay(500);
   digitalWrite(ddPin, HIGH);
-  delay(2000);
-    //Initialize Roomba SCI
-  // Start SCI
-  pinSerial.write(128);
-  delay(100);
-  // Enable control
-  pinSerial.write(130);  
-  delay(100);
-  // Enable full control, no safety, all commands
-  pinSerial.write(132);
-  delay(100);
-
 }
--- python/connectionTest.py
+++ python/connectionTest.py
@@ -10,9 +10,13 @@ ser = serial.Serial(port='/dev/ttyACM0',
 if not ser.isOpen():
     ser.open()
     print "opened serial"
+else:
+    print "Serial was open"
 
 blinkRed = [139, 16, 255, 255]
 blinkGreen = [139, 32, 0, 255]
+
+SCI = [128, 130, 132]
 
 #  send variable amount of integers to serialport
 def sendToRoomba(integers):
@@ -21,6 +25,10 @@ def sendToRoomba(integers):
         sendString += " "+str(num) # add x to end parseint()
     ser.write(sendString)
     time.sleep(0.1)
+
+print "enable full control"
+sendToRoomba(SCI)
+time.sleep(2)
 
 print "Enable red"
 sendToRoomba(blinkRed)
